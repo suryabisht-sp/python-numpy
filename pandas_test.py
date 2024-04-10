77ed91859ddcd559c4c7412541e21a269b2bdc41
suryabisht-sp <surya_bisht@softprodigy.com>
2024-04-10 19:27:27 +0530

    pandas interpolate

--- pandas_test.py
+++ pandas_test.py
@@ -56,10 +56,108 @@ x1=x.pop('b')
 
 """
 writing the csv file 
-
+will produce the csv file in the current folder, index=false will remove the index from the file
 """
 
 dis={1:[1,5,3,6,9,], 2:[4,5,6,9,5], "d":[4,5,69,8,5]}
 
 x= pd.DataFrame(dis)
-print(x)
+# x.to_csv("csv1.csv",index=False)
+'it will produce the header with given new name in the file'
+# x.to_csv("csv1.csv",index=False, header=["a","b","c"])  
+
+# print(x)
+
+
+"""
+reading the csv file using pandas
+"""
+
+# csv_file=pd.read_csv('//home//user//Documents//python//first//csv1.csv', usecols=['Name'])
+# csv_file=pd.read_csv('//home//user//Documents//python//first//csv1.csv', nrows=1)
+# csv_file=pd.read_csv('//home//user//Documents//python//first//csv1.csv', skiprows=[0,1,4])
+# csv_file=pd.read_csv('//home//user//Documents//python//first//csv1.csv', index_col='Name')
+'will make particular row as header'
+# csv_file=pd.read_csv('//home//user//Documents//python//first//csv1.csv', header=2)
+'make particular heading'
+# csv_file=pd.read_csv('//home//user//Documents//python//first//csv1.csv', names=["Surya's Data","Kesh","har"])
+# csv_file=pd.read_csv('//home//user//Documents//python//first//csv1.csv', header=None)
+# csv_file=pd.read_csv('//home//user//Documents//python//first//csv1.csv', dtype={'class':"float"})
+"will show the range of index"
+csv_file=pd.read_csv('//home//user//Documents//python//first//csv1.csv')
+# print(csv_file.index)
+'will show the names of all the columns'
+# ""print(csv_file.columns)
+"to find min, max 50% 70%"
+# print(csv_file.describe())
+"starting 5 dataa"
+# print(csv_file.head())
+"last five"
+# print(csv_file.tail(2))
+"want to see particular range data"
+# print(csv_file[2:4])
+"show index in an array"
+# print(csv_file.index.array)
+"convert the data in numpy array"
+# print(csv_file.to_numpy())
+"sorting the data"
+# print(csv_file.sort_index(axis=0, ascending=False))
+"changing the in the column"
+# x=csv_file["Name"][0]="Surya"
+# print(x)
+# print(csv_file)
+# y=csv_file.loc[0,"Name"]="Kesh"
+# y=csv_file.loc[[1,2],["Name", "class"]]
+# print(y)
+# print(csv_file)
+"locating and priting particular data and then whole column"
+# y=csv_file.iloc[0,0]
+y=csv_file.drop("class", axis=1)
+# print(y)
+
+"""
+handling the missing data using dropna and fillna
+"""
+'will drop the blank values or row'
+# print(csv_file.dropna())
+# print(csv_file.dropna(axis=1))
+# print(csv_file.dropna(axis=0))
+"if any row blank or blank data it will remove the row with how='any'"
+# print(csv_file.dropna(how="any"))
+"if use all it will remove the blank row only"
+# print(csv_file.dropna(how="all"))
+"will remove single null value or acc to given value"
+# print(csv_file.dropna(thresh=1))
+"fill na value with data"
+# print(csv_file.fillna("all"))
+"making condition to fill data in columns"
+# print(csv_file.fillna({"Name":"all", "class":"first", "status":"suspended"}))
+"filling the empty with the previous data of row"
+# print(csv_file.fillna(method="ffill", axis=1))
+# print(csv_file.fillna({"Name":"all", "class":"first", "status":"suspended"}))
+
+
+
+"""
+  handling the missing data replace and interpolate
+"""
+
+# print(csv_file.replace(to_replace=3, value=8))
+# print(csv_file.replace(to_replace="ssss", value="kesh"))
+# print(csv_file.replace("[A-Za-z]", "Saurya", regex=True))
+# x=csv_file.replace({"Name":'[A-Za-z]'},22, regex=True)
+'replacing 2 with backward filling'
+# x=csv_file.replace(3, method="bfill")
+# x=csv_file.replace(1, method="bfill", limit=1)
+"inplace it will not create a copy of the data instead make the modification in the data itself"
+# x=csv_file.replace(1, method="bfill", limit=1, inplace=True)
+"interpolate work with only numeric value"
+# x=csv_file.interpolate()
+"must have same data type in col for axis 1"
+# x=csv_file.interpolate(method="linear", axis=0)
+# x=csv_file.interpolate(limit_direction="backward", limit=2)
+"outside will fil all na numeic value while inside will keep the na data"
+# x=csv_file.interpolate(limit_area="outside")
+"replacing the data in orignal"
+# x=csv_file.interpolate(limit_direction="forward", limit=3, inplace=True)
+# print(csv_file)
